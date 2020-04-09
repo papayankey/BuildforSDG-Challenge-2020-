40a044cc909a0801d57ce2ce1a4fbf5d2f4855df
iambenbright <yankeybenneth@gmail.com>
2020-04-09 12:22:37 +0000

    build first challenge

--- src/estimator.js
+++ src/estimator.js
@@ -1,8 +1,59 @@
-const covid19ImpactEstimator = data => {
+// estimate currently infected
+function estimateInfected({ reportedCases, numberOfInfected }) {
+  return reportedCases * numberOfInfected;
+}
+
+// normalise duration in days
+function normaliseDurationInDays({ periodType, timeToElapse }) {
+  switch (periodType) {
+    case 'weeks':
+      return timeToElapse * 7;
+    case 'months':
+      return timeToElapse * 31;
+    default:
+      return timeToElapse;
+  }
+}
+
+// estimate infections by requested time
+function estimateInfectionsByRequestedTime({
+  currentlyInfected,
+  durationInDays
+}) {
+  const infectionsDoublePeriodInDays = 3;
+  return Math.floor(
+    currentlyInfected *
+      Math.pow(2, durationInDays / infectionsDoublePeriodInDays)
+  );
+}
+
+// estimate cases
+function estimateCases(data, numberInfected) {
+  const { reportedCases, periodType, timeToElapse } = data;
+
+  const durationInDays = normaliseDurationInDays({ periodType, timeToElapse });
+
+  // challenge 1 codes
+  const currentlyInfected = estimateInfected({ reportedCases, numberInfected });
+  const infectionsByRequestedTime = estimateInfectionsByRequestedTime({
+    currentlyInfected,
+    durationInDays
+  });
+
+  return {
+    currentlyInfected,
+    infectionsByRequestedTime
+  };
+}
+
+const covid19ImpactEstimator = (data) => {
+  const impactInfected = 10;
+  const severeImpactInfected = 50;
+
   return {
     data,
-    impact: {},
-    severeImpact: {}
+    impact: estimateCases({ data, impactInfected }),
+    severeImpact: estimateCases({ data, severeImpactInfected })
   };
 };
 
